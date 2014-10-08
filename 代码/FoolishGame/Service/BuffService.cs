3a7cfc4c81ed8afa2f311e37346dc90b9d4cead1
Loki Meow <inuyasya030@163.com>
2014-10-08 15:49:50 +0800

    asd

--- 代码/FoolishGame/Service/BuffService.cs
+++ 代码/FoolishGame/Service/BuffService.cs
@@ -5,18 +5,7 @@ namespace Service
 {
     public static class BuffService
     {
-        #region 角色Buff增加与移除方法
-        public static Charactor UpdateBuff(Charactor charactor, Buff buff, bool isGood = true) {
-            return PropertyService.UpdateProperty(charactor, buff, buff.NumericalType, isGood);
-        }
-
-        public static List<Charactor> UpdateBuff(List<Charactor> charactorList, Buff buff, bool isGood = true) {
-            var result = new List<Charactor>();
-            foreach (var item in charactorList) {
-                result.Add(UpdateBuff(item, buff, isGood));
-            }
-            return result;
-        }
-        #endregion
+        
     }
 }
+ 
